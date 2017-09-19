8a395d6b0f74ad5e910317081214523bcb8c19a7
qin <qin@qindeMac-mini.local>
2017-09-19 16:43:18 +0800

    Initial Commit

---
+++ star/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  star
+//
+//  Created by qin on 2017/9/19.
+//  Copyright © 2017年 qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
